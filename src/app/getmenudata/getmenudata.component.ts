63bbbe975e750e92b6d91d2b40548889e4bbfcdc
Nicolasabar <abarcanicolas6@gmail.com>
2023-11-12 20:40:31 -0300

    validacion largo nombre

--- src/app/getmenudata/getmenudata.component.ts
+++ src/app/getmenudata/getmenudata.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
 import { MatTreeNestedDataSource, MatTreeModule} from '@angular/material/tree';
 import { MatIconModule} from '@angular/material/icon';
 import { MatButtonModule} from '@angular/material/button';
@@ -8,7 +8,7 @@ import { MatInputModule} from '@angular/material/input';
 import { MatGridListModule} from '@angular/material/grid-list';
 import { ApiService } from '../service/api.service';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 
 interface FoodNode {
   name: string;
@@ -22,7 +22,7 @@ interface FoodNode {
   templateUrl: './getmenudata.component.html',
   styleUrls: ['./getmenudata.component.css'],
   standalone: true,
-  imports: [MatTreeModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatGridListModule, FormsModule ]
+  imports: [CommonModule, MatTreeModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatGridListModule, FormsModule ]
 })
 export class GetmenudataComponent implements OnInit {
 
@@ -35,7 +35,11 @@ export class GetmenudataComponent implements OnInit {
   precio: number = 0;
   id_padre: number = 0;
 
-  constructor(private apiService: ApiService) {
+  mensajeValidacion: string = '';
+  botonBloqueado: boolean = false;
+  nombreValido: boolean = true;
+
+  constructor(private apiService: ApiService, private zone: NgZone) {
     
   }
 
@@ -64,10 +68,43 @@ export class GetmenudataComponent implements OnInit {
   
   
   addCategory() {
-    this.apiService.addCategory(this.nombre, this.precio, this.id_padre).subscribe((response) => {
-      console.log(response); // Puedes manejar la respuesta de la API según tus necesidades
-    });
+    if (this.validarNombre() && this.validarLargoNombre()) {
+      this.apiService.addCategory(this.nombre, this.precio, this.id_padre).subscribe((response) => {
+        console.log(response); // Puedes manejar la respuesta de la API según tus necesidades
+      });
+      this.mensajeValidacion = '';
+      this.botonBloqueado = false;
+    } else {
+      if (!this.validarNombre()) {
+        this.mensajeValidacion = 'Nombre inválido. Solo se permiten letras.';
+      } else {
+        this.mensajeValidacion = 'Nombre demasiado largo. Máximo 10 caracteres.';
+      }
+      this.botonBloqueado = true;
+    }
+  
+    // Forzamos la actualización de la vista dentro de la zona de Angular
+    this.zone.run(() => {});
+  }
+  
+
+  validarLargoNombre(): boolean {
+    return this.nombre.length <= 10;
   }
 
+
+  actualizarEstadoBoton() {
+    this.botonBloqueado = !(this.validarNombre() && this.validarLargoNombre());
+  }
+
+
+  validarNombre(): boolean {
+  // Expresión regular que acepta al menos un carácter, letras (mayúsculas y minúsculas), y espacios en blanco
+  const regex = /^[a-zA-Z\s]+$/;
+  this.nombreValido = regex.test(this.nombre) && this.validarLargoNombre();
+  return this.nombreValido;
+  }
+  
+  
   hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
 }
